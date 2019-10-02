3cb5d04ac23d723a6f886cc65a22205f400679c1
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-02 15:58:29 +0100

    MVP Completed

--- src/index.js
+++ src/index.js
@@ -7,6 +7,36 @@ import Navbar from "./components/Navbar";
 
 import "./styles.scss";
 
+const useLocalStorage = (key, initialValue) =>{
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  const setValue = value => {
+    // Save state
+    setStoredValue(value);
+    // Save to local storage
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+  return [storedValue, setValue];
+};
+
+export const useDarkMode = (initialValue) => {
+  const [someValue, setSomeValue] = useLocalStorage('name', initialValue);
+
+  useEffect(() => {
+    const bodyElement = document.querySelector('body');
+    if(someValue){
+      bodyElement.classList.add('dark-mode');
+    }else{
+      bodyElement.classList.remove('dark-mode');
+    }
+  },[someValue]);
+
+  return [ someValue, setSomeValue]
+};
+
 const App = () => {
   const [coinData, setCoinData] = useState([]);
 
